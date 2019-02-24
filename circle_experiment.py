809f44ddb41ad3fa63d1131c3a8c8291c21c7cab
JGuymont <j.guymont@gmail.com>
2019-02-24 10:19:17 -0500

    Clean code and pep8

--- circle_experiment.py
+++ circle_experiment.py
@@ -1,202 +1,113 @@
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.optimize as opt
 import random
 import time
 
+from utils.dataset import Dataset
+from utils.dataloader import DataLoader
+from utils import visualize
 from mlp.mlp import MLPClassifier
-from mlp.dataloader import DataLoader
+import constants
 
-CIRCLE_DATA_PATH = './data/circles/circles.txt'
 
-class Data:
-    """Abstract class for the circles dataset
+def finite_difference_check(dataset, batch_size):
+    """
+    Computes the gradients for a single example, and
+    check that the gradient is correct using the nite
+    difference method.
 
-    Args
-        path: (string) path to the dataset
-        input_dim: (int)
-        split: (list) list of float [train_pct, valid_pct, test_pct]
+    Answers to questions 1, 2, and 4.
+    """
+    dataloader = DataLoader(dataset, batch_size)
+
+    inputs, targets = next(dataloader)
+
+    mlp = MLPClassifier(constants.Circles.INPUT_DIM, constants.Circles.N_CLASSES)
+    gradHats, grads, param_names = mlp.finite_difference_check(inputs, targets)
+
+    figure_name = 'finite_difference_check_batch_size_{}.png'.format(batch_size)
+
+    visualize.plot_gradient(
+        gradHats, grads,
+        param_names,
+        legend=['finite differences approx.', 'backpropagation'],
+        path=os.path.join(constants.Circles.FIGURES_DIR, figure_name)
+    )
+
+
+def decision_boundaries(train_data, valid_data):
+    """
+    Question 5: Train the neural network using gradient descent on the two circles dataset.
+    Plot the decision regions for several different values of the hyperparameters
+    (weight decay, number of hidden units, early stopping) so as to illustrate their
+    effect on the capacity of the model.
     """
 
-    def __init__(self, path, input_dim, split):
-        self._raw_data = np.loadtxt(open(path, 'r'))
-        self._input_dim = input_dim
-        self._data = self._read_data()
-        self._data_index = self._split_index(split)
+    # raw data is only used to plot the decision boundary
+    raw_data = np.loadtxt(open(constants.Circles.DATA_PATH, 'r'))
+    X = raw_data[:, :2]
+    y = raw_data[:, -1]
 
-    def __len__(self):
-        return len(self._data)
+    # hyperparameters
+    HIDDEN_DIM_SET = [8, 14]
+    NUM_EPOCH_SET = [30]
+    LEARNING_RATE_SET = [0.05]
+    L1_WEIGH_DECAY = [0, 0.005]
+    L2_WEIGH_DECAY = [0, 0.005]
 
-    def __getitem__(self, i):
-        return self._data[i]
+    trainloader = DataLoader(train_data, batch_size=32)
+    devloader = DataLoader(valid_data, batch_size=len(valid_data))
 
-    def _read_data(self):
-        inputs = self._raw_data[:, :self._input_dim]
-        targets = self._raw_data[:, -1].astype(int)
-        return list(zip(inputs, targets))
+    i = 0
+    for h in HIDDEN_DIM_SET:
+        for lr in LEARNING_RATE_SET:
+            for l1 in L1_WEIGH_DECAY:
+                for l2 in L2_WEIGH_DECAY:
+                    for n_epoch in NUM_EPOCH_SET:
 
-    def _split_index(self, split):
-        storage = {'train': [], 'valid': [], 'test': []}
-        train_size = round(len(self)*split[0])
-        valid_size = round((len(self) - train_size)*split[1])
+                        print('\nhidden_dim: {}, lr: {}, l1: {}, l2: {}'.format(h, lr, l1, l2))
+                        mlp = MLPClassifier(constants.Circles.INPUT_DIM, constants.Circles.N_CLASSES, h, lr, n_epoch, l1, l2, l1, l2)
+                        mlp.train(trainloader, devloader)
 
-        examples = range(len(self))
-        storage['train'] = random.sample(examples, train_size)
-        examples = [ex for ex in examples if ex not in storage['train']] # remove index
-        storage['valid'] = random.sample(examples, valid_size)
-        storage['test'] = [ex for ex in examples if ex not in storage['valid']]
-        return storage
+                        figure_name = 'decision_boundaries_{}.png'.format(i)
 
-    def train(self):
-        return [self._data[i] for i in self._data_index['train']]
+                        visualize.plot_decision(
+                            X, y,
+                            path=os.path.join(constants.Circles.FIGURES_DIR, figure_name),
+                            model=mlp,
+                            param=[h, lr, n_epoch, l1, l2, l1, l2]
+                        )
+                        i += 1
 
-    def valid(self):
-        return [self._data[i] for i in self._data_index['valid']]
 
-    def test(self):
-        return [self._data[i] for i in self._data_index['test']]
+def main_experiment(train_data, valid_data):
+    """
+    Question 8:
+    """
+    trainloader = DataLoader(train_data, batch_size=32)
+    devloader = DataLoader(valid_data, batch_size=len(valid_data))
 
-    def dim_(self):
-        return self._input_dim
+    mlp = MLPClassifier(constants.Circles.INPUT_DIM, constants.Circles.N_CLASSES, 10, 0.05, 50)
+    mlp.train(trainloader, devloader)
 
-def plot_gradient(grad1, grad2, param_names, legend, title):
-    plt.rcParams.update({'font.size': 6})
-    plt.plot(param_names, grad1, '--')
-    plt.plot(param_names, grad2, 'o')
-    plt.legend(legend)
-    plt.xlabel('parameter')
-    plt.ylabel('gradient')
-    plt.savefig(title)
-    plt.show()
 
 if __name__ == '__main__':
 
-    INPUT_DIM = 2
-    OUTPUT_DIM = 2
+    train_data = Dataset(
+        path=constants.Circles.TRAIN_PATH,
+        input_features=constants.Circles.INPUT_FEATURES,
+        output_features=constants.Circles.OUTPUT_FEATURES
+    )
 
-    data = Data(CIRCLE_DATA_PATH, input_dim=2, split=[0.7, 0.15, 0.15])
+    valid_data = Dataset(
+        path=constants.Circles.VALID_PATH,
+        input_features=constants.Circles.INPUT_FEATURES,
+        output_features=constants.Circles.OUTPUT_FEATURES
+    )
 
-    def question12():
-        batch = DataLoader(data.train(), batch_size=1)
-        X = batch[0][0]
-        Y = batch[0][1]
-        
-        mlp = MLPClassifier(INPUT_DIM, OUTPUT_DIM)
-        gradHats, grads, param_names = mlp.finite_difference_check(X, Y, crazy_loop=True)
-
-        plot_gradient(
-            gradHats, grads, 
-            param_names, 
-            legend=['finite differences approx.', 'backpropagation'],
-            title='plots/question2.jpg'
-        )
-        
-    def question4():
-        batch = DataLoader(data.train(), batch_size=10)
-        X = batch[20][0]
-        Y = batch[20][1]
-
-        mlp = MLPClassifier(INPUT_DIM, OUTPUT_DIM)
-        gradHats, grads, param_names = mlp.finite_difference_check(X, Y, crazy_loop=True)
-
-        plot_gradient(
-            gradHats, grads, 
-            param_names, 
-            legend=['finite differences approx.', 'backpropagation'],
-            title='plots/question4.jpg'
-        )
-
-    def question5():
-
-        def get_boudary():
-            mlp = MLPClassifier(INPUT_DIM, OUTPUT_DIM)
-            data = np.loadtxt(open(CIRCLE_DATA_PATH, 'r'))[:, :2]
-            
-            boundary_x = []
-            boundary_y = []
-
-            for x in data:
-                sol = opt.root(mlp.forward_root, x)
-                boundary_x.append(sol['x'][0])
-                boundary_y.append(sol['x'][1])
-            
-            plt.scatter(boundary_x, boundary_y)
-            plt.show()
-
-        raw_data = np.loadtxt(open(CIRCLE_DATA_PATH, 'r'))[:, :2]
-
-        # hyperparameters
-        BATCH_SIZE = 32
-        NUM_EPOCH = 1000
-
-        HIDDEN_DIM_SET = [10]
-        LEARNING_RATE_SET = [0.05]
-        L1_WEIGH_DECAY = [0.001]
-        L2_WEIGH_DECAY = [0.001]
-        
-        trainloader = DataLoader(data.train(), batch_size=BATCH_SIZE)
-        devloader = DataLoader(data.valid(), batch_size=len(data.valid()))
-
-        for h in HIDDEN_DIM_SET:
-            for lr in LEARNING_RATE_SET:
-                for l1 in L1_WEIGH_DECAY:
-                    for l2 in L2_WEIGH_DECAY:
-                
-                        print('\nhidden_dim: {}, lr: {}, l1: {}, l2: {}'.format(h, lr, l1, l2))
-                        mlp = MLPClassifier(INPUT_DIM, OUTPUT_DIM, h, lr, NUM_EPOCH, l1, l2, l1, l2)
-                        mlp.train(trainloader, devloader, crazy_loop=False)
-
-                        boundary_x1 = list(np.arange(-1, 1, 0.01))
-                        boundary_x2 = []
-
-                        for x1 in boundary_x1:
-                            
-                            sol = opt.root(mlp.forward_root, 0, x1=x1)
-                            boundary_x2.append(sol['x'])
-        
-                        plt.scatter(boundary_x1, boundary_x2)
-        plt.show()
-
-    def question7():
-
-        trainloader = DataLoader(data.train(), batch_size=1)
-
-        #print(*trainloader[0])
-
-
-        mlp = MLPClassifier(INPUT_DIM, OUTPUT_DIM)
-        
-        mlp.backward(*trainloader[0], crazy_loop=False)
-        grads, param_names = mlp.get_gradients()
-
-        mlp.backward(*trainloader[0], crazy_loop=True)
-        grad_loop, param_names = mlp.get_gradients()
-
-        plot_gradient(
-            grads, grad_loop, 
-            param_names, 
-            legend=['Matrix calculus', 'Loop'],
-            title='plots/question71.jpg'
-        )
-
-        trainloader = DataLoader(data.train(), batch_size=10)
-        mlp = MLPClassifier(INPUT_DIM, OUTPUT_DIM)
-        
-        mlp.backward(*trainloader[0], crazy_loop=False)
-        grads, param_names = mlp.get_gradients()
-
-        mlp.backward(*trainloader[0], crazy_loop=True)
-        grad_loop, param_names = mlp.get_gradients()
-
-        plot_gradient(
-            grads, grad_loop, 
-            param_names, 
-            legend=['Matrix calculus', 'Loop'],
-            title='plots/question72.jpg'
-        )
-
-    #question12()
-    #question4()
-    #question5()
-    #question7()
-    
+    finite_difference_check(train_data, batch_size=1)
+    finite_difference_check(train_data, batch_size=10)
+    decision_boundaries(train_data, valid_data)
+    main_experiment(train_data, valid_data)
